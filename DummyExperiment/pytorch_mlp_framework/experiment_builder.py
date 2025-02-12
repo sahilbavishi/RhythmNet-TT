eca595b9988f84d92c87063589310741ee85e2d9
Matthew Pugh <s2742733@ed.ac.uk>
2025-02-12 14:37:16 +0000

    Missed some areas to change to GPU - fixed

--- DummyExperiment/pytorch_mlp_framework/experiment_builder.py
+++ DummyExperiment/pytorch_mlp_framework/experiment_builder.py
@@ -64,7 +64,7 @@ class CreateHeartbeatWindow(nn.Module):
         #Now get the start positions and the end positions of the windows:
         StartEnd=InputPositions[:,:-1,:]*self.sigma+(1-self.sigma)*InputPositions[:,1:,:]
         #Append a 0 to the start
-        StartEnd=torch.cat((torch.zeros([StartEnd.shape[0],1,1]),StartEnd),dim=1)
+        StartEnd=torch.cat((torch.zeros([StartEnd.shape[0],1,1]).to(self.device),StartEnd),dim=1)
         #Make sure we are not giving too wide a window
         StartEnd[StartEnd>self.maxtimeframe]=self.maxtimeframe
         #Return the start and end position by concating them on the 2nd dimension
@@ -77,7 +77,7 @@ class GboxIoULoss(nn.Module):
     A class that does the box gIoU loss function, but also deals with the range for a heart beat to be in.
     This is based off the paper we are doing our base model on
     """
-    def __init__(self,delta=float(10e-7),maxtimeframe=1080):
+    def __init__(self,device,delta=float(10e-7),maxtimeframe=1080):
         """
         Initialise the gboxiou loss module
 
@@ -91,6 +91,7 @@ class GboxIoULoss(nn.Module):
         super(GboxIoULoss,self).__init__()
         self.delta=delta
         self.maxtimeframe=maxtimeframe
+        self.device=device
     def __call__(self, Preds,TargetWindows):
         """
         Calculate the loss, by creating the windows and then using the loss function
@@ -106,7 +107,7 @@ class GboxIoULoss(nn.Module):
         out1,out2=Preds[:,:,0],Preds[:,:,1]
         tgt1,tgt2=TargetWindows[:,:,0],TargetWindows[:,:,1]
         #Calculate intersetctions,unions and areas
-        Intersection=torch.max(torch.zeros_like(out1),torch.min(out2,tgt2)-torch.max(out1,tgt1))
+        Intersection=torch.max(torch.zeros_like(out1).to(self.device),torch.min(out2,tgt2)-torch.max(out1,tgt1))
         Union=tgt2-tgt1+out2-out1-Intersection #Union is the size of both minus the intersection between them
         Area=torch.max(tgt2,out2)-torch.min(tgt1,out1) #This is the shortest area that encompases both windows
 
