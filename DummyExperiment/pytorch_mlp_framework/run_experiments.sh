b2c1e952645bffeb046d2ee5c1b35f1fe2367cbc
Matthew Pugh <pugh.m2002@yahoo.com>
2025-03-02 11:16:48 +0000

    Ran model with updated F1 scores

--- DummyExperiment/pytorch_mlp_framework/run_experiments.sh
+++ DummyExperiment/pytorch_mlp_framework/run_experiments.sh
@@ -2,18 +2,18 @@
 source ~/miniconda3/bin/activate
 conda activate mlp
 
-python train_evaluate_image_classification_system.py --batch_size 32   --learning_rate 0.0001   --num_epochs 200   --experiment_name BaseLR0001TH1HU12   --use_gpu True --transformer_heads 1 --hidden_units 12 --dataset_path "/home/s2742733/mlp/MLP_CW/MIT3sec.csv"
+#python train_evaluate_image_classification_system.py --batch_size 32   --learning_rate 0.0001   --num_epochs 200   --experiment_name BaseLR0001TH1HU12   --use_gpu True --transformer_heads 1 --hidden_units 12 --dataset_path "/home/s2742733/mlp/MLP_CW/MIT3sec.csv"
 
-python train_evaluate_image_classification_system.py --batch_size 32   --learning_rate 0.0001   --num_epochs 200   --experiment_name BaseLR0001TH1HU24   --use_gpu True --transformer_heads 1 --hidden_units 24 --dataset_path "/home/s2742733/mlp/MLP_CW/MIT3sec.csv"
+#python train_evaluate_image_classification_system.py --batch_size 32   --learning_rate 0.0001   --num_epochs 200   --experiment_name BaseLR0001TH1HU24   --use_gpu True --transformer_heads 1 --hidden_units 24 --dataset_path "/home/s2742733/mlp/MLP_CW/MIT3sec.csv"
 
-python train_evaluate_image_classification_system.py --batch_size 32   --learning_rate 0.0001   --num_epochs 200   --experiment_name BaseLR0001TH2HU12   --use_gpu True --transformer_heads 2 --hidden_units 12 --dataset_path "/home/s2742733/mlp/MLP_CW/MIT3sec.csv"
+#python train_evaluate_image_classification_system.py --batch_size 32   --learning_rate 0.0001   --num_epochs 200   --experiment_name BaseLR0001TH2HU12   --use_gpu True --transformer_heads 2 --hidden_units 12 --dataset_path "/home/s2742733/mlp/MLP_CW/MIT3sec.csv"
 
-python train_evaluate_image_classification_system.py --batch_size 32   --learning_rate 0.0001   --num_epochs 200   --experiment_name BaseLR0001TH2HU24   --use_gpu True --transformer_heads 2 --hidden_units 24 --dataset_path "/home/s2742733/mlp/MLP_CW/MIT3sec.csv"
+#python train_evaluate_image_classification_system.py --batch_size 32   --learning_rate 0.0001   --num_epochs 200   --experiment_name BaseLR0001TH2HU24   --use_gpu True --transformer_heads 2 --hidden_units 24 --dataset_path "/home/s2742733/mlp/MLP_CW/MIT3sec.csv"
 
-python train_evaluate_image_classification_system.py --batch_size 32   --learning_rate 0.0001   --num_epochs 200   --experiment_name BaseLR0001TH3HU12   --use_gpu True --transformer_heads 3 --hidden_units 12 --dataset_path "/home/s2742733/mlp/MLP_CW/MIT3sec.csv"
+#python train_evaluate_image_classification_system.py --batch_size 32   --learning_rate 0.0001   --num_epochs 200   --experiment_name BaseLR0001TH3HU12   --use_gpu True --transformer_heads 3 --hidden_units 12 --dataset_path "/home/s2742733/mlp/MLP_CW/MIT3sec.csv"
 
-python train_evaluate_image_classification_system.py --batch_size 32   --learning_rate 0.0001   --num_epochs 200   --experiment_name BaseLR0001TH3HU24   --use_gpu True --transformer_heads 3 --hidden_units 24 --dataset_path "/home/s2742733/mlp/MLP_CW/MIT3sec.csv"
+#python train_evaluate_image_classification_system.py --batch_size 32   --learning_rate 0.0001   --num_epochs 200   --experiment_name BaseLR0001TH3HU24   --use_gpu True --transformer_heads 3 --hidden_units 24 --dataset_path "/home/s2742733/mlp/MLP_CW/MIT3sec.csv"
 
-python train_evaluate_image_classification_system.py --batch_size 32   --learning_rate 0.0001   --num_epochs 200   --experiment_name BaseLR0001TH6HU12   --use_gpu True --transformer_heads 6 --hidden_units 12 --dataset_path "/home/s2742733/mlp/MLP_CW/MIT3sec.csv"
+#python train_evaluate_image_classification_system.py --batch_size 32   --learning_rate 0.0001   --num_epochs 200   --experiment_name BaseLR0001TH6HU12   --use_gpu True --transformer_heads 6 --hidden_units 12 --dataset_path "/home/s2742733/mlp/MLP_CW/MIT3sec.csv"
 
 python train_evaluate_image_classification_system.py --batch_size 32   --learning_rate 0.0001   --num_epochs 200   --experiment_name BaseLR0001TH6HU24   --use_gpu True --transformer_heads 6 --hidden_units 24 --dataset_path "/home/s2742733/mlp/MLP_CW/MIT3sec.csv"
