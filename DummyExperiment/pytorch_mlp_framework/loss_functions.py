5af429b0eee3f8fc9d26fd0366b18c1598c7d96d
Alessandro Ciano <s2659893@uhtred.inf.ed.ac.uk>
2025-03-22 03:36:21 +0000

    nan loss fixed

--- DummyExperiment/pytorch_mlp_framework/loss_functions.py
+++ DummyExperiment/pytorch_mlp_framework/loss_functions.py
@@ -27,14 +27,12 @@ class FocalLoss(nn.Module):
 
         RealVals: One-hot encodings of the correct classes
         """
-        #First, we are going to get the correct classes from the realvals
         real_classes=torch.argmax(RealVals,dim=-1)
-        #Get the probabilities predicted of the targets
         Ps=torch.gather(Preds,dim=2,index=torch.unsqueeze(real_classes,2))
-        #Calc loss:
+        Ps = torch.clamp(Ps, min=1e-10, max=1 - 1e-10)  # Prevent log(0) or log(negative)
         Loss=-self.theta*(1-Ps)**2*torch.log(Ps)
 
-        return torch.sum(Loss) #Return the sum of the losses
+        return torch.sum(Loss)
 
 class CreateHeartbeatWindow(nn.Module):
     def __init__(self, maxtimeframe,sigma,device):
@@ -48,17 +46,11 @@ class CreateHeartbeatWindow(nn.Module):
 
         InputPositons size: [Batchsize, NumberofClassifications, 1] (The positions are at the end)
         """
-        #To do this, I am first going to change any 0s to above the maximum length, as they are at the end
         InputPositions[InputPositions==0]=self.maxtimeframe*2
-        #Append one more as well
         InputPositions=torch.cat((torch.unsqueeze(InputPositions,2),self.maxtimeframe*2*torch.ones([InputPositions.shape[0],1,1]).to(self.device)),dim=1)
-        #Now get the start positions and the end positions of the windows:
         StartEnd=InputPositions[:,:-1,:]*self.sigma+(1-self.sigma)*InputPositions[:,1:,:]
-        #Append a 0 to the start
         StartEnd=torch.cat((torch.zeros([StartEnd.shape[0],1,1]).to(self.device),StartEnd),dim=1)
-        #Make sure we are not giving too wide a window
         StartEnd[StartEnd>self.maxtimeframe]=self.maxtimeframe
-        #Return the start and end position by concating them on the 2nd dimension
         StartEnd=torch.cat((StartEnd[:,:-1,:],StartEnd[:,1:,:]),dim=2)
         return StartEnd
 
@@ -94,15 +86,12 @@ class GboxIoULoss(nn.Module):
         Targets: The actual windows of the heartbeats
         """
 
-        #Get the start of window and end of window
         out1,out2=Preds[:,:,0],Preds[:,:,1]
         tgt1,tgt2=TargetWindows[:,:,0],TargetWindows[:,:,1]
-        #Calculate intersetctions,unions and areas
         Intersection=torch.max(torch.zeros_like(out1).to(self.device),torch.min(out2,tgt2)-torch.max(out1,tgt1))
-        Union=tgt2-tgt1+out2-out1-Intersection #Union is the size of both minus the intersection between them
+        Union=tgt2-tgt1+out2-out1-Intersection + 1e-7 #Union is the size of both minus the intersection between them
         Area=torch.max(tgt2,out2)-torch.min(tgt1,out1) #This is the shortest area that encompases both windows
 
-        #Calc loss
         Loss=1-(Intersection/Union-(Area-Union)/(Area+self.delta))
         
 
